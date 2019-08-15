6936a245c3f41648abeb7f1983d2fddb63047cf6
ronnie <ron@acctechnology.ph>
2019-08-15 10:41:29 +0800

    Init

---
+++ BackUpDatabase/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BackUpDatabase
+{
+    public class Configuration
+    {
+
+        public static string ConnectionString = 
+            GetItem(AppDomain.CurrentDomain.BaseDirectory + @"\Settings.ini", "ConnectionString");
+        public static string Destination = 
+            GetItem(AppDomain.CurrentDomain.BaseDirectory + @"\Settings.ini", "Destination");
+        public static string DatabaseName = 
+            GetItem(AppDomain.CurrentDomain.BaseDirectory + @"\Settings.ini", "Databasename");
+
+        public static string GetItem(string path, string Identifier)
+        {
+            StreamReader file = new StreamReader(path);
+            string Result = "";
+            do
+            {
+                string Line = file.ReadLine();
+                if (Line.ToLower().StartsWith(Identifier.ToLower() + "="))
+                {
+                    Result = Line.Substring(Identifier.Length + 2);
+                }
+            } while (file.Peek() != -1);
+            return Result;
+        }
+    }
+    
+}
---
+++ BackUpDatabase/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+
+
+namespace BackUpDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ConnectionString = Configuration.ConnectionString;
+            ConnectionString = ConnectionString.Replace("\\\\","\\");
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection { ConnectionString = ConnectionString })
+                {
+                    sqlConnection.Open();
+
+                    Console.WriteLine("Creating back up file....");
+                    Thread.Sleep(2000);
+                    string path = $@"BACKUP DATABASE ""{Configuration.DatabaseName}"" TO DISK='{Path.Combine(Configuration.Destination,
+                                   "navy_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".bak")}'";
+                    SqlCommand sqlCommand = new SqlCommand(path, sqlConnection);
+                    sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+
+                    Console.WriteLine($"Successful......");
+                    Console.WriteLine();
+                } 
+
+            } catch (Exception)
+            {
+                Console.WriteLine("Error cannot connect to the database...");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Current Path Location: " + AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine("Back Up Database Location  : " + Configuration.Destination);
+            Console.WriteLine("Database Name : "+ Configuration.DatabaseName);
+            Console.WriteLine("ConnectionString " + Configuration.ConnectionString);
+            Thread.Sleep(1000);
+            Console.WriteLine("bye...");
+            Thread.Sleep(200);
+        }
+    }
+}
